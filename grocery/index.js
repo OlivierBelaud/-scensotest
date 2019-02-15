f257e573ffd6232ff4800fd09fea73b9d7c4e2d7
OlivierBelaud <obelaud@fluks.com>
2019-02-15 16:43:02 +0100

    initial commit

---
+++ grocery/index.js
@@ -0,0 +1,6 @@
+const sillyname = require('sillyname');
+const apple = require('@olivierbelaud/apple');
+const banana = require('@olivierbelaud/banana');
+console.log(`grocery and ${sillyname()}`);
+console.log(apple);
+console.log(banana);
